950863649e07ae76992945cea27881a9d4651122
Song Chinn <c.songchinn@gmail.com>
2026-04-21 18:35:56 +0800

    test

--- cypress/stepdef/common.js
+++ cypress/stepdef/common.js
@@ -3,7 +3,6 @@ import { Given, When, Then, And } from "@badeball/cypress-cucumber-preprocessor"
 Given('I navigate to Mars Airlines homepage', ()=>{
     cy.visit('https://marsair.recruiting.thoughtworks.net/SongChinn')
     cy.url().should('include', '/SongChinn')
-    cy.task('summaryLog', "✅ Homepage looks good");
 });
 
 Then('I took a screenshot of the homepage', ()=>{
