d4e124e30ea095a8382087a50e09b5d3f0459031
Song Chinn <c.songchinn@gmail.com>
2026-04-21 18:41:50 +0800

    test

--- report.js
+++ report.js
@@ -1,11 +1,19 @@
 const report = require('multiple-cucumber-html-reporter');
 
 report.generate({
-	jsonDir: './cypress/reports/',
-	reportPath: './cypress/reports/cucumber-htmlreport.html',
-	metadata:{
-        browser: { name: 'chrome', version: 'latest' },
-        device: 'Local test machine',
-        platform: { name: 'ubuntu', version: 'latest' }
+  jsonDir: './cypress/reports/json/', // Must match the output in package.json
+  reportPath: './cypress/reports/html/',
+  metadata: {
+    device: 'Local Test Machine',
+    platform: {
+      name: 'windows' // or 'ubuntu' for GitHub Actions
     }
+  },
+  customData: {
+    title: 'Cypress Cucumber Run Info',
+    data: [
+      { label: 'Project', value: 'My Demo Project' },
+      { label: 'Cycle', value: 'Regression' }
+    ]
+  }
 });
